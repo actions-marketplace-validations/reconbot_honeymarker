d520fae50636a61d269920a6e7528fa2183a3b3a
Francis Gulotta <wizard@roborooter.com>
2021-10-07 01:06:39 -0400

    more testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,16 @@
 set -euxo pipefail
 
 function marker_url() {
-    if [ -z "$INPUT_URL" ] ; then
+    if [ -z "$INPUT_URL" ]
+    then
         curl -s "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}" | jq -r '.html_url'
     else
-        echo "$INPUT_URL"
+        echo $INPUT_URL
     fi
 }
 
 function create_marker() {
-    honeymarker -k "$INPUT_APIKEY" -d "$INPUT_DATASET" add -t "$INPUT_TYPE" -m "$INPUT_MESSAGE" -u "$INPUT_URL"
+    honeymarker -k "$INPUT_APIKEY" -d "$INPUT_DATASET" add -t "$INPUT_TYPE" -m "$INPUT_MESSAGE" -u "$(marker_url)"
 }
 
 create_marker
